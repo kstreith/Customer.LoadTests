5a82be238a50dacc49d01c7520e59994398373e2
Kip <kstreith@yahoo.com>
2019-09-26 20:13:25 -0400

    Update load tests to create consumer and read from random consumers

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,13 +1,38 @@
-import os
+from os import getenv
 from locust import HttpLocust, TaskSet, task
 from faker import Faker
+from uuid import uuid4
+import requests
+import random
+
+def get_fake_customer():
+    fake = Faker('en_US')
+    return {
+        "FirstName": fake.first_name(),
+        "LastName": fake.last_name(),
+        "EmailAddress": "test%s@loadtest.itsnull.com" % (uuid4())
+    }
 
 class ApiTasks(TaskSet):
-    @task(1)
+    @task(4)
     def getCustomer(self):
-        self.client.get("/api/customer/88f37d26-6616-4598-8792-e3bb9b814c72")
+        self.client.get("/api/customer/%s" % (random.choice(consumer_ids)) , name="Get /api/consumer")
+
+    @task(1)
+    def createCustomer(self):
+        self.client.post("/api/customer/", json=get_fake_customer(), name="Post /api/consumer")
+
+def get_testconsumerids(host):
+    response = requests.get(host + "/api/loadtest/randomcustomer")
+    responseObj = response.json()
+    print("responseObj", dir(responseObj))
+    print("values", responseObj["values"])
+    return responseObj["values"]
+
+host = getenv("api_host")
+consumer_ids = get_testconsumerids(host)
 
 class ApiUser(HttpLocust):
-    host = os.getenv("api_host")
+    host = host
     task_set = ApiTasks
     min_wait = max_wait = 1000
