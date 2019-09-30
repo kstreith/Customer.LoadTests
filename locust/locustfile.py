b96e9178a7eca73d20b1397177d6877d4f094526
Streithorst <kip.streithorst@appliedis.com>
2019-09-29 20:15:05 -0400

    Add load for 400 requests

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -13,6 +13,17 @@ def get_fake_customer():
         "EmailAddress": "test%s@loadtest.itsnull.com" % (uuid4())
     }
 
+def get_invalid_fake_customer():
+    fake_invalid_customer = get_fake_customer()
+    value = random.randint(1, 3)
+    if value == 1:
+        fake_invalid_customer["FirstName"] = None
+    elif value == 2:
+        fake_invalid_customer["LastName"] = None
+    else:
+        fake_invalid_customer["EmailAddress"] = None
+    return fake_invalid_customer
+
 class ApiTasks(TaskSet):
     @task(4)
     def getCustomer(self):
@@ -22,9 +33,21 @@ class ApiTasks(TaskSet):
     def createCustomer(self):
         self.client.post("/api/customer/", json=get_fake_customer(), name="Post /api/consumer")
 
+    @task(1)
+    def createCustomerInvalid(self):
+        with self.client.post("/api/customer/", json=get_invalid_fake_customer(), name="Post Invalid /api/customer", catch_response=True) as response:
+            if response.status_code == 400:
+                response.success()
+
     @task(2)
     def updateConsumer(self):
         self.client.put("/api/customer/%s" % (random.choice(consumer_ids)), json=get_fake_customer(), name="Put /api/consumer")
+
+    @task(1)
+    def updateCustomerInvalid(self):
+        with self.client.put("/api/customer/%s" % (random.choice(consumer_ids)), json=get_invalid_fake_customer(), name="Put Invalid /api/customer", catch_response=True) as response:
+            if response.status_code == 400:
+                response.success()
 
 def get_testconsumerids(host):
     response = requests.get(host + "/api/loadtest/randomcustomer")
