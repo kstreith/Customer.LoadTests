755e337bc1cbc1cc24c68a71f43043c5678e7849
Streithorst <kip.streithorst@appliedis.com>
2019-09-30 22:16:37 -0400

    Add user

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -5,6 +5,12 @@ from uuid import uuid4
 import requests
 import random
 
+users = ["estore","mobileapp","retailstore"]
+
+def get_headers():
+    headers = {"x-fake-user": random.choice(users)}
+    return headers
+
 def get_fake_customer():
     fake = Faker('en_US')
     hasAnyBirthFields = random.randint(1, 2)
@@ -39,37 +45,37 @@ def get_invalid_fake_customer():
 class ApiTasks(TaskSet):
     @task(16)
     def getCustomer(self):
-        self.client.get("/api/customer/%s" % (random.choice(consumer_ids)), name="Get /api/consumer")
+        self.client.get("/api/customer/%s" % (random.choice(consumer_ids)), headers=get_headers(), name="Get /api/consumer")
 
     @task(1)
     def getCustomerNotFound(self):
-        with self.client.get("/api/customer/%s" % (uuid4()), name="Get NotFound /api/consumer", catch_response=True) as response:
+        with self.client.get("/api/customer/%s" % (uuid4()), headers=get_headers(), name="Get NotFound /api/consumer", catch_response=True) as response:
             if response.status_code == 404:
                 response.success()
 
     @task(4)
     def createCustomer(self):
-        self.client.post("/api/customer/", json=get_fake_customer(), name="Post /api/consumer")
+        self.client.post("/api/customer/", headers=get_headers(), json=get_fake_customer(), name="Post /api/consumer")
 
     @task(1)
     def createCustomerInvalid(self):
-        with self.client.post("/api/customer/", json=get_invalid_fake_customer(), name="Post Invalid /api/customer", catch_response=True) as response:
+        with self.client.post("/api/customer/", headers=get_headers(), json=get_invalid_fake_customer(), name="Post Invalid /api/customer", catch_response=True) as response:
             if response.status_code == 400:
                 response.success()
 
     @task(8)
     def updateConsumer(self):
-        self.client.put("/api/customer/%s" % (random.choice(consumer_ids)), json=get_fake_customer(), name="Put /api/consumer")
+        self.client.put("/api/customer/%s" % (random.choice(consumer_ids)), headers=get_headers(), json=get_fake_customer(), name="Put /api/consumer")
 
     @task(1)
     def updateCustomerInvalid(self):
-        with self.client.put("/api/customer/%s" % (random.choice(consumer_ids)), json=get_invalid_fake_customer(), name="Put Invalid /api/customer", catch_response=True) as response:
+        with self.client.put("/api/customer/%s" % (random.choice(consumer_ids)), headers=get_headers(), json=get_invalid_fake_customer(), name="Put Invalid /api/customer", catch_response=True) as response:
             if response.status_code == 400:
                 response.success()
 
     @task(1)
     def updateCustomerNotFound(self):
-        with self.client.put("/api/customer/%s" % (uuid4()), json=get_fake_customer(), name="Put NotFound /api/customer", catch_response=True) as response:
+        with self.client.put("/api/customer/%s" % (uuid4()), headers=get_headers(), json=get_fake_customer(), name="Put NotFound /api/customer", catch_response=True) as response:
             if response.status_code == 404:
                 response.success()
 
